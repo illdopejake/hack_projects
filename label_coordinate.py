5eaef8127a8b9852cc333a2ec3e9380de7d48c9e
Jake Vogel <jacobwvogel@gmail.com>
2018-04-28 14:23:00 -0400

    post hackathon update

---
+++ label_coordinate.py
@@ -0,0 +1,132 @@
+import pandas
+import itertools
+import numpy as np
+import nibabel as ni
+from scipy import stats
+
+
+def label_coordinate_by_atlas(atlas, coordinates, cube_size = 1):
+    ''' This function will take a set of coordinates and an atlas and return the
+    atlas label for each coordinate. Optionally, a cube can be drawn around the 
+    coordinate. In this case, the most frequent non-zero value inside the cube will 
+    be selected as the label. The function will output a pandas dataframe with a
+    label for each input coordinate.
+    
+    atlas can be a path to a nifti image, or a nifti object, or a 3D np.ndarray
+    
+    coordinates can be a pandas Dataframe, a numpy.ndarray or a list of lists, 
+    representing the mni coordinates for each input corrdinate. as such, the 
+    length of one of the dimensions should be 3
+    
+    cube_size represents the radius of the cube to be created. leaving it as 1 will
+    only retrieve the value at the coordinate specified. Increasing the value will 
+    draw a cube of increasing size and collect values from within the cube. The most
+    frequent non-zero value within the cube will be selected
+    
+    '''
+    
+    # initiate and check inputs
+    atl, coords = init_and_chk_inputs(atlas,coordinates)
+    
+    # convert coordinates to xyz space
+    xyz = np.array([convert_coords(coords[x],'xyz') for x in range(coords.shape[0])
+                   ]).astype(int)
+    
+    # create output container
+    results = pandas.DataFrame(index=range(coords.shape[0]),columns = ['Label'])
+    
+    # get labels for each coordinate
+    print('extracting labels')
+    if cube_size == 1:
+        results.loc[:,'Label'] = atl[xyz[:,0].tolist(),xyz[:,1].tolist(),xyz[:,2].tolist()]
+    else:
+        labs = []
+        for i in range(xyz.shape[0]):
+            labs.append(extract_value_from_cube(xyz[i,:], cube_size, atl))
+        results.loc[:,'Label'] = labs
+    
+    print('completed')
+    print('%s coordinates were outside of the provided atlas'%(
+                                            len(results[results.Label==0])))
+
+    return results
+    
+def init_and_chk_inputs(atlas,coordinates):
+    print('checking and initializing inputs')
+    if type(atlas) == str:
+        atl = ni.load(atlas).get_data()
+    elif type(atlas) == nibabel.nifti1.Nifti1Image: 
+        atl = atlas.get_data()
+    elif type(atlas) == numpy.core.memmap.memmap or type(atlas) == np.ndarray:
+        atl = atlas
+    else:
+        raise IOError('atlas must be a nifti object, a numpy array or a path to a nifti file')
+    
+    if type(coordinates) == pandas.core.frame.DataFrame:
+        coords = coordinates.values
+       
+    elif type(coordinates) == list or type(coordinates) == tuple:
+        if len(coordinates)== 3:
+            coords = np.zeros((len(coordinates[0]),3))
+            for i in range(3):
+                coords[:,i] = coords[i]
+    elif type(coordinates) == np.ndarray:
+        coords = coordinates
+    else:
+        raise IOError('coordiantes must be a pandas dataframe, numpy array or list of lists')
+    
+    if not any(x==3 for x in coords.shape):
+        raise IOError('Coordinates are 3D and thus 3 columns are expected...')
+    elif coords.shape[-1] != 3:
+        coords = coords.T
+    
+    coords = coords.round()
+    
+    return atl,coords
+    
+def convert_coords(coord, to_what = 'mni', vs = 1):
+    origin = [90, 126, 72]
+    origin = (np.array(origin) / vs).astype(int).tolist()
+    x,y,z = coord[0],coord[1],coord[2]
+    
+    if to_what == 'mni':
+        x = (origin[0]-x)*vs
+        y = (y-origin[1])*vs
+        z = (z-origin[2])*vs
+        
+    elif to_what == 'xyz':
+        x=origin[0]- x/vs
+        y=y/vs + origin[1]
+        z=z/vs + origin[2]
+        
+    else:
+        raise IOError('please set to_what to either mni or xyz')
+    
+    return x,y,z
+
+def extract_value_from_cube(coord, radius, atl):
+    
+    xs,ys,zs = make_cube(coord, radius)
+    cube_vals = atl[xs,ys,zs]
+    if stats.mode(cube_vals)[0][0] == 0:
+        if len(cube_vals[cube_vals!=0]) > 0:
+            lab = stats.mode(cube_vals[cube_vals!=0])[0][0]
+        else:
+            lab = 0
+    else:
+        lab = stats.mode(cube_vals)[0][0]
+    
+    return lab
+        
+def make_cube(coord, radius):
+    summers = []
+    negrad = (radius*-1)+1
+    for x in itertools.product(range(negrad,radius),repeat=3):
+        summers.append(np.array(x))
+    s_coords = [np.array(coord) + x for x in summers]
+    xs = [int(x[0]) for x in s_coords]
+    ys = [int(x[1]) for x in s_coords]
+    zs = [int(x[2]) for x in s_coords]
+    
+    return xs, ys, zs
+
