3cca0190398a208e142e8b92144225cbe006ba9c
Jake Vogel <jacobwvogel@gmail.com>
2018-05-17 13:29:46 -0400

    some noise and updated a bug on medications script

--- medications.py
+++ medications.py
@@ -75,44 +75,44 @@ def redundancy removal(allmeds,inwords=None)
 def populate_spreadsheet(med_subdf,medf)
     #med_subdf should have subject as index...
     # in its current state this won't work
-typedf = pandas.DataFrame(np.zeros((len(sdf.index),len(medf.Type.unique()))),index=sdf.index,columns = medf.Type.unique())
-stdf = pandas.DataFrame(np.zeros((len(sdf.index),len(medf.Spec_type.unique()))),index=sdf.index,columns = medf.Spec_type.unique())
-usedf = pandas.DataFrame(np.zeros((len(sdf.index),len(medf.Use.unique()))),index=sdf.index,columns = medf.Use.unique())
-for sub in newdf.index:
-    row = newdf.ix[sub,'medications']
-    for entry in row.split(','):
-        entry = mstrip(entry)
-        suggs = difflib.get_close_matches(entry,med_lib,cutoff=0.7)
-        if len(suggs) == 1:
-            choice = suggs[0]
-        if len(suggs) > 1:
-            cnt = 0
-            for i in range(len(suggs)-1):
-                if all(medf.loc[suggs[i+1]][1:4] == medf.loc[suggs[0]][1:4]):
-                    cnt += 1
-            if cnt == len(suggs)-1:
+    typedf = pandas.DataFrame(np.zeros((len(sdf.index),len(medf.Type.unique()))),index=sdf.index,columns = medf.Type.unique())
+    stdf = pandas.DataFrame(np.zeros((len(sdf.index),len(medf.Spec_type.unique()))),index=sdf.index,columns = medf.Spec_type.unique())
+    usedf = pandas.DataFrame(np.zeros((len(sdf.index),len(medf.Use.unique()))),index=sdf.index,columns = medf.Use.unique())
+    for sub in newdf.index:
+        row = newdf.ix[sub,'medications']
+        for entry in row.split(','):
+            entry = mstrip(entry)
+            suggs = difflib.get_close_matches(entry,med_lib,cutoff=0.7)
+            if len(suggs) == 1:
                 choice = suggs[0]
-            else:
-                print('Whoops.. im not sure.')
-                print('my best guess for %s is %s'%(entry,suggs[0]))
-                notok = input('if this is correct, press enter. For other options, type no and press enter:')
-                if notok:
-                    print('Okay, here are all my guesses \n',suggs)
-                    usr = input('Please enter the position (0 to %s) of the best answer:'%(len(suggs)-1))
-                    choice = suggs[int(usr)]
-                    print('chose %s'%(choice))
-            t = medf.ix[choice,'Type']                                                                                     
-            s = medf.ix[choice,'Spec_type']                                 
-            u = medf.ix[choice,'Use']
-            typedf.ix[sub,t] = typedf.ix[sub,t] + 1
-            stdf.ix[sub,s] = stdf.ix[sub,s] + 1
-            usedf.ix[sub,u] = usedf.ix[sub,u] + 1
+            if len(suggs) > 1:
+                cnt = 0
+                for i in range(len(suggs)-1):
+                    if all(medf.loc[suggs[i+1]][1:4] == medf.loc[suggs[0]][1:4]):
+                        cnt += 1
+                if cnt == len(suggs)-1:
+                    choice = suggs[0]
+                else:
+                    print('Whoops.. im not sure.')
+                    print('my best guess for %s is %s'%(entry,suggs[0]))
+                    notok = input('if this is correct, press enter. For other options, type no and press enter:')
+                    if notok:
+                        print('Okay, here are all my guesses \n',suggs)
+                        usr = input('Please enter the position (0 to %s) of the best answer:'%(len(suggs)-1))
+                        choice = suggs[int(usr)]
+                        print('chose %s'%(choice))
+                t = medf.ix[choice,'Type']                                                                                     
+                s = medf.ix[choice,'Spec_type']                                 
+                u = medf.ix[choice,'Use']
+                typedf.ix[sub,t] = typedf.ix[sub,t] + 1
+                stdf.ix[sub,s] = stdf.ix[sub,s] + 1
+                usedf.ix[sub,u] = usedf.ix[sub,u] + 1
 
-            # for next lines, need to add part that finds and get rid of nans
-            ncol = ['use_'+x for x in usedf.columns]
-            usedf.columns = ncol
-            ncol = ['type_'+x for x in typedf.columns]
-            typedf.columns = ncol
-            ncol = ['spectype_'+x for x in stdf.columns]
-            stdf.columns = ncol
-            
+                # for next lines, need to add part that finds and get rid of nans
+                ncol = ['use_'+x for x in usedf.columns]
+                usedf.columns = ncol
+                ncol = ['type_'+x for x in typedf.columns]
+                typedf.columns = ncol
+                ncol = ['spectype_'+x for x in stdf.columns]
+                stdf.columns = ncol
+                
